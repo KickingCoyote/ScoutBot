219f52875e3d350bfc4d8ca47796415629ef8f78
lemomoment <kickingcoyote2@gmail.com>
2024-09-17 16:03:32 +0200

    Började göra en lista med alla olika move-kombinationer

---
+++ Assets/Scripts/Spel/Everymove.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Everymove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            for (int u = 0; u <=1; u++)
+            {
+                while (i == 1)
+                {
+                    u = 1;
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
