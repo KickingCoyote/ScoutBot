37cd7a547e7ec0c8249942c8ace1ad971b12086c
polin-06 <polin_06@edu.sollentuna.se>
2024-09-19 11:26:26 +0200

    La till kod som skriver alla möjliga drag i unitys console

--- Assets/Scripts/Spel/Everymove.cs
+++ Assets/Scripts/Spel/Everymove.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,16 +8,37 @@ public class Everymove : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //for (int i = 0; i < 10; i++)
-        //{
-        //    for (int u = 0; u <=1; u++)
-        //    {
-        //        while (i == 1)
-        //        {
-        //            u = 1;
-        //        }
-        //    }
-        //}
+        for (int i = 1; i <= 10; i++)
+        {
+            for (int u = 0; u <= 1; u++)
+            {
+                if (i == 1)
+                {
+                    u = 1;
+                }
+                if (u == 0)
+                {
+                    for (int v = 1; v <= (10-(i-1)); v++)
+                    {
+                        Debug.Log((i-1) + " " + u + " " + (v-1));
+                    }
+                }
+                else
+                {
+                    for (int v = 1; v <= 10; v++)
+                    {
+                        if (i == 10 && u == 1)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            Debug.Log((i - 1) + " " + u + " " + (v - 1));
+                        }
+                    }
+                }
+            }
+        }
     }
 
     // Update is called once per frame
