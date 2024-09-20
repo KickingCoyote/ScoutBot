dd3cbef687d12af655b54062154be5543330d9f5
KickingCoyote <kickingcoyote2@gmail.com>
2024-09-20 16:57:09 +0200

    Recoded the Entire back end of scout with the new optimizations
    Created everything related to moves and move generation in the new optimized version

--- Assets/Scripts/Spel/GameBase.cs
+++ Assets/Scripts/Spel/GameBase.cs
@@ -1,9 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameBase : MonoBehaviour
 {
+
+    private string inputString;
+
+
+
+    //Used for UI
+    [SerializeField] TextMeshProUGUI[] pText = new TextMeshProUGUI[4];
+    [SerializeField] TextMeshProUGUI infoText;
+
+
     void Start()
     {
         DistributeCards();
@@ -13,7 +24,7 @@ public class GameBase : MonoBehaviour
     {
         for (int i = 0; i < 44; i++)
         {
-            SBU.cards[i] = i % 4 + 8 * Random.Range(0, 2) + 16 * (i / 4);
+            SBU.cards[i] = i % 4 + 1 + 8 * Random.Range(0, 2) + 16 * (i / 4);
         }
         SBU.cards = SBU.ShuffleCards(SBU.cards);
 
@@ -23,7 +34,37 @@ public class GameBase : MonoBehaviour
     private void GameUpdate()
     {
         SBU.turn++;
+        UpdateGUI();
     }
 
+
+
+
+    //Activated from buttons ingame
+    public void TakeCard()
+    {
+
+
+
+        GameUpdate();
+    }
+
+    public void PutCard()
+    {
+
+        GameUpdate();
+    }
+
+    //ran every input field is deselected
+    public void UpdateString(string s)
+    {
+        inputString = s;
+    }
+
+
+    private void UpdateGUI()
+    {
+
+    }
 
 }
--- Assets/Scripts/Spel/SBU.cs
+++ Assets/Scripts/Spel/SBU.cs
@@ -2,13 +2,16 @@ using JetBrains.Annotations;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
 
 public static class SBU
 {
 
-    // 0000, 0, 000. handPos, flipped, owner
+    // 0000, 0, 000. (hand) index, flip, owner
 
     public static int[] cards = new int[44];
 
@@ -35,6 +38,28 @@ public static class SBU
     }
 
 
+    //Flip a card upside down
+    public static int FlipCardValue(int card)
+    {
+        //Convert card into bit array
+        byte[] a = new byte[1] { BitConverter.GetBytes(card)[0] };
+        BitArray b = new BitArray(a);
+
+        //Swaps around the bit array
+        for (int i = 0; i < b.Length / 2; i++)
+        {
+            bool temp = b[i];
+            b[i] = b[i + 4];
+            b[i + 4] = temp;
+        }
+
+        //convert back the bit array to a integer
+        b.CopyTo(a, 0);
+
+        return a[0];
+
+    }
+
 
     //Returns the upwards facing value on a card
     public static int getCurrentCardValue(int cardValue)
@@ -49,18 +74,183 @@ public static class SBU
     }
 
 
-    public static void Move(int[] cards, int[] move)
+    //The following functions may not work with negative numbers
+    /// <summary>
+    /// Get the hand index of a card
+    /// </summary>
+    /// <param name="card">the card</param>
+    /// <returns></returns>
+    public static int getCardIndex(int card)
     {
-        for (int i = 0; i < 44; i++)
+        //Uses same logic as getCurrentCardValue
+        return getCurrentCardValue(card);
+    }
+    public static int getCardOwner(int card)
+    {
+
+        byte[] b = new byte[1] { BitConverter.GetBytes(card)[0] };
+        BitArray bits = new BitArray(b);
+        for (int i = 3; i < 8; i++)
         {
-            cards[i] += move[i];
+            bits[i] = false;
         }
+        bits.CopyTo(b, 0);
+
+        return b[0];
+    }
+    public static int getCardFlip(int card)
+    {
+        byte[] b = new byte[1] { BitConverter.GetBytes(card)[0] };
+        BitArray bits = new BitArray(b);
+
+        if (bits[3]) { return 1; }
+        return 0;
+
     }
 
 
-    public static void GetPossibleMoves(int player, int[] cards)
+    public static int[] Move(int[] cards, int[] move)
+    {
+        for (int i = 0; i < move.Length; i++)
+        {
+            cards[i] += move[i];
+        }
+
+        return cards;
+    }
+
+
+    //doom, fix this sphagetti code
+    //Move Generation for putting down cards
+    public static int[,] GetPossibleMoves(int player, int[] cards)
     {
 
+        //Adds the players card into an array sorted like it is in the players hand, all empty spots are -10
+        int[] pCards = getPlayerCards(cards, player);
+        
+        //Get all the possible moves
+        List<int[]> temp = new List<int[]>();
+
+        for (int i = 0; i < pCards.Length; i++)
+        {
+            temp.Add(new int[1] {pCards[i]});
+
+            for (int j = 1; j < pCards.Length - 1; j++)
+            {
+                if (getCurrentCardValue(getValueOfCard(pCards[i])) == getCurrentCardValue(getValueOfCard(pCards[i + j])))
+                {
+                    for (int k = 0; k < j + 1; k++)
+                    {
+                        int[] move = new int[j + 1];
+                        move[k] = pCards[i + k];
+                        temp.Add(move);
+                    }                    
+                }
+                else { break; }
+            }
+            for (int j = 1; j < pCards.Length - 1; j++)
+            {
+                if (getCurrentCardValue(getValueOfCard(pCards[i])) == getCurrentCardValue(getValueOfCard(pCards[i + j])) - 1)
+                {
+                    for (int k = 0; k < j + 1; k++)
+                    {
+                        int[] move = new int[j + 1];
+                        move[k] = pCards[i + k];
+                        temp.Add(move);
+                    }
+                }
+                else { break; }
+            }
+            for (int j = 1; j < pCards.Length - 1; j++)
+            {
+                if (getCurrentCardValue(getValueOfCard(pCards[i])) == getCurrentCardValue(getValueOfCard(pCards[i + j])) + 1)
+                {
+                    for (int k = 0; k < j + 1; k++)
+                    {
+                        int[] move = new int[j + 1];
+                        move[k] = pCards[i + k];
+                        temp.Add(move);
+                    }
+                }
+                else { break; }
+            }
+        }
+
+
+        //reformat the moves
+        int[,] moves = new int[44, temp.Count];
+
+        for (int i = 0; i < temp.Count; i++)
+        {
+            int[] move = GenerateMove(cards, temp.ElementAt(i), player);
+
+            for (int j = 0; j < 44; j++) { moves[j, i] = move[j]; }
+
+        }
+
+
+        return moves;
+    }
+
+    /// <summary>
+    /// Generates a move array from a set of cards (move) and a GameState
+    /// </summary>
+    /// <param name="cards">the global cards (GameState)</param>
+    /// <param name="move">the set of cards that the move consists of, must be in order</param>
+    /// <returns></returns>
+    public static int[] GenerateMove(int[] cards, int[] move, int player)
+    {
+
+
+        int[] pCards = getPlayerCards(cards, player); 
+
+        int[] moveArray = new int[44];
+
+        int k = 0;
+        bool foundMove = false;
+        for (int j = 0; j < pCards.Length; j++)
+        {
+            if (pCards[j] == move[k])
+            {
+                //Move card to center pile and set its new index in center pile
+                moveArray[move[k]] = 16 * (k - getCardIndex(cards[move[k]])) - player;
+
+                foundMove = true;
+                k++;
+            }
+            else if (foundMove)
+            {
+                //Reduce index by move.length;
+                moveArray[pCards[j]] = -16 * move.Length;
+
+            }
+
+        }
+
+        return moveArray;
+    }
+
+    /// <summary>
+    /// Adds the players card into an array sorted like it is in the players hand, all empty spots are -10
+    /// </summary>
+    /// <param name="cards"></param>
+    /// <param name="player"></param>
+    /// <returns></returns>
+    private static int[] getPlayerCards(int[] cards, int player)
+    {
+        int[] pCards = new int[15];
+        for (int i = 0; i < 15; i++) { pCards[i] = -10; }
+
+        for (int i = 0; i < cards.Length; i++)
+        {
+            if (getCardOwner(cards[i]) == player)
+            {
+                pCards[getCardIndex(cards[i])] = i;
+            }
+
+        }
+
+        return pCards;
     }
 
 
@@ -87,7 +277,7 @@ public static class SBU
     {
         for (int i = 0; i < 44; i++)
         {
-            if (cardValues[i] == value)
+            if (getValueOfCard(i) == value)
             {
                 return i;
             }
@@ -97,12 +287,22 @@ public static class SBU
         Debug.Log("Invalid Value");
         return -1;
     }
-
+    public static int getValueOfCard(int card)
+    {
+        if (getCardFlip(card) == 0)
+        {
+            return cardValues[card];
+        }
+        else
+        {
+            return FlipCardValue(cardValues[card]);
+        }
+    }
 
 
     public static string CardToString(int card)
     {
-        int value = cardValues[card];
+        int value = getValueOfCard(card);
         return getCurrentCardValue(value) + ":" + (value - (16 * getCurrentCardValue(value)));
     }
     public static int CardFromString(string cardStr)
@@ -110,5 +310,35 @@ public static class SBU
         string[] str = cardStr.Split(':');
         return getCardFromValue(int.Parse(str[0]) * 16 + int.Parse(str[1]));
     }
+
+
+    //gets the points of a move
+    public static int moveValue(int[] move)
+    {
+
+        int length = 0;
+        int startingValue = -1;
+        int isMatch = 1;
+        int previousCard = -2;
+
+        for (int i = 0; i < move.Length; i++)
+        {
+            //if the card owner isnt 0 means that the card owner is being changed. aka that card is being moved
+            if (getCardOwner(move[i]) != 0)
+            {
+                if (startingValue == -1) { startingValue = getCurrentCardValue(getValueOfCard(i)); }
+                length++;
+                if(i != move.Length - 1 && getCurrentCardValue(getValueOfCard(i)) == getCurrentCardValue(getValueOfCard(previousCard)) + 1)
+                {
+                    isMatch = 0;
+                }
+
+                previousCard = i;
+            }
+        }
+
+        return (length - 1) * 100 + isMatch * 10 + startingValue - 1;
+    }
+
 
 }
